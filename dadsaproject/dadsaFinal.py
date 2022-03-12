968623f48cc79056a0cf13b58451c88f757ce7ac
Shunan <shunanshareef1@gmail.com>
2022-03-12 14:57:03 +0500

    Initial commit

---
+++ dadsaproject/dadsaFinal.py
@@ -0,0 +1,65 @@
+class Node:
+    def __init__(self, value=None, next=None):
+        self.value = value # head is the node from which your are pointing towards next node
+        self.next = next # next is the next node where pointer is pointing
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        
+    def append(self, element):
+        node = Node(element, self.head)
+        self.head = node
+        
+    def insert_at_end(self, element):
+        if self.head is None:
+            node = Node(element, None)
+            self.head = node
+            return
+        itr = self.head
+        while itr.next:
+            itr = itr.next
+        itr.next = Node(element, None)
+
+    def print(self):
+        itr = self.head
+        linked_list = ''
+        while itr:
+            linked_list += str(itr.value) + " "
+            itr = itr.next
+        print(linked_list)
+        
+    def length(self):
+        itr = self.head
+        count = 0  
+        while itr:
+            count += 1  
+            itr = itr.next
+        return count
+        
+    def value_at(self, index):
+        itr = self.head
+        count = 0
+        value = ''  
+        while itr:
+            if count == index:
+                value = itr.value
+            count += 1
+            itr = itr.next
+        return value
+        
+    def replace_at(self, index, element):
+        if self.head is None:
+            print("linked list is empty")
+            return
+        if index > (self.length()-1):
+            print("index is out of range")
+            return
+        itr = self.head
+        count = 0
+        while itr:
+            if count == index:
+                itr.value = element
+                break
+            itr = itr.next
+            count += 1
